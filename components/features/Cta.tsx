49482981cae51094216e494eb9dfc16c992e0891
Kuranga Elizabeth <elizabethak24@outlook.com>
2024-11-23 12:20:26 +0100

    Add montserrat font

--- components/features/Cta.tsx
+++ components/features/Cta.tsx
@@ -14,7 +14,7 @@ const Cta = () => {
 		<div className={`py-32 bg-cta bg-cover bg-center`}>
 			<div className="flex flex-col w-fit mx-auto">
 				<div className="px-16 py-12 flex flex-col md:justify-between md:flex-row justify-center  gap-28 bg-white ">
-					<section className="max-w-80">
+					<section className="max-w-[500px]">
 						<h2 className="text-h2 text-center md:text-left">{text}</h2>
 						<p className="py-4">{message}</p>
 						<Button text={btn} variant="primary" />
--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -1,32 +1,30 @@
-import { section } from '@/constants';
+import { section } from "@/constants";
 
 export default function Heading({
-  caption,
-  text,
-  section: state,
+	caption,
+	text,
+	section: state,
 }: HeadingProps) {
-  return (
-    <div className="flex flex-col gap-4 justify-center text-center">
-      {/* <h1 className="text-h1 text-center my-5">{children}</h1>; */}
-
-      {state === section.main ? (
-        <>
-          <p>{caption.toUpperCase()}</p>
-          <h2 className="text-h2 text-black">{text}</h2>
-        </>
-      ) : state === section.gallery ? (
-        <>
-          <p>{caption.toUpperCase()}</p>
-          <h4 className="text-h4 text-black">{text}</h4>
-        </>
-      ) : (
-        <div className="space-y-4 text-left">
-          <p className="text-[12px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-gradient-100 to-gradient-200">
-            {caption.toUpperCase()}
-          </p>
-          <h5 className="text-h5 text-left text-black font-bold">{text}</h5>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-4 justify-center text-center mx-auto max-w-[750px]">
+			{state === section.main ? (
+				<>
+					<p>{caption.toUpperCase()}</p>
+					<h2 className="text-h2 text-black">{text}</h2>
+				</>
+			) : state === section.gallery ? (
+				<>
+					<p>{caption.toUpperCase()}</p>
+					<h4 className="text-h4 text-black">{text}</h4>
+				</>
+			) : (
+				<div className="space-y-4 text-left">
+					<p className="text-[12px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-gradient-100 to-gradient-200">
+						{caption.toUpperCase()}
+					</p>
+					<h5 className="text-h5 text-left text-black font-bold">{text}</h5>
+				</div>
+			)}
+		</div>
+	);
 }
