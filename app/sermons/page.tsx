e8ecac5169b798766cecefaed76073d89b009d01
Yunus Habeeb <habeebdh1@gmail.com>
2024-12-29 13:11:06 +0100

    Refactor Header component to conditionally apply background based on caption; update imports for consistency

--- app/sermons/page.tsx
+++ app/sermons/page.tsx
@@ -1,30 +1,32 @@
-import Container from "@/components/Container";
-import Footer from "@/components/Footer";
-import UpcomingSermons from "@/components/features/UpcomingSermons";
-import BlogPosts from "@/components/features/BlogPosts";
-import Image from "next/image";
+import Container from '@/components/Container';
+import Footer from '@/components/Footer';
+import UpcomingSermons from '@/components/features/UpcomingSermons';
+import BlogPosts from '@/components/features/BlogPosts';
+import Header from '@/components/features/Header';
+import { sermon } from '@/constants';
 
-const Sermons = () => (
-	<main>
-		<Image
-			src="/sermon/sermon.svg"
-			alt="About US"
-			height="1280"
-			width="512"
-			className="w-full"
-		/>
-		<Container>
-			<UpcomingSermons />
-		</Container>
+const Sermons = () => {
+  const {
+    heading: { text, caption },
+  } = sermon;
 
-		<Container>
-			<BlogPosts />
-		</Container>
+  return (
+    <main>
+      <Header text={text} caption={caption} />
 
-		<Container containerClass="bg-[#161722] text-white">
-			<Footer />
-		</Container>
-	</main>
-);
+      <Container>
+        <UpcomingSermons />
+      </Container>
+
+      <Container>
+        <BlogPosts />
+      </Container>
+
+      <Container containerClass="bg-[#161722] text-white">
+        <Footer />
+      </Container>
+    </main>
+  );
+};
 
 export default Sermons;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,46 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-	darkMode: ["class"],
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#FFD2A4",
-				"primary-hover": "#F2C79B",
-				secondary: "#0D0D0D",
-				"secondary-black": "#161722",
-				"secondary-orange": "#FFF5EB",
-				grey: "#F5F2F099",
-				"light-orange": "#FFF5EB",
-				"gradient-100": "#A54E2B",
-				"gradient-200": "#DC9853",
-			},
-			backgroundColor: {
-				gradient: "linear-gradient(#A54E2B, DC9853)",
-			},
-			fontSize: {
-				h1: ["clamp(2.5rem, 4vw, 4rem)", "1.2"], // 40px to 64px
-				h2: ["clamp(2rem, 3.5vw, 3rem)", "1.2"], // 32px to 48px
-				h3: ["clamp(1.75rem, 3vw, 2.5rem)", "1.2"], // 28px to 40px
-				h4: ["clamp(1.5rem, 2.5vw, 2rem)", "1.2"], // 24px to 32px
-				h5: ["clamp(1.25rem, 2vw, 1.5rem)", "1.2"], // 20px to 24px
-				h6: ["clamp(1rem, 1.5vw, 1rem)", "1.2"], // 16px fixed
-				btn: ["1rem", "1.2"], // Fixed 16px
-			},
-			backdropBlur: {
-				"4xl": "80px",
-			},
-			backgroundImage: {
-				cta: "url('/cta/bg.svg')",
-				"hero-about": "url('/about/hero.png')",
-			},
-		},
-	},
-	plugins: [require("tailwindcss-animate")],
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FFD2A4',
+        'primary-hover': '#F2C79B',
+        secondary: '#0D0D0D',
+        'secondary-black': '#161722',
+        'secondary-orange': '#FFF5EB',
+        grey: '#F5F2F099',
+        'light-orange': '#FFF5EB',
+        'gradient-100': '#A54E2B',
+        'gradient-200': '#DC9853',
+      },
+      backgroundColor: {
+        gradient: 'linear-gradient(#A54E2B, DC9853)',
+      },
+      fontSize: {
+        h1: ['clamp(2.5rem, 4vw, 4rem)', '1.2'], // 40px to 64px
+        h2: ['clamp(2rem, 3.5vw, 3rem)', '1.2'], // 32px to 48px
+        h3: ['clamp(1.75rem, 3vw, 2.5rem)', '1.2'], // 28px to 40px
+        h4: ['clamp(1.5rem, 2.5vw, 2rem)', '1.2'], // 24px to 32px
+        h5: ['clamp(1.25rem, 2vw, 1.5rem)', '1.2'], // 20px to 24px
+        h6: ['clamp(1rem, 1.5vw, 1rem)', '1.2'], // 16px fixed
+        btn: ['1rem', '1.2'], // Fixed 16px
+      },
+      backdropBlur: {
+        '4xl': '80px',
+      },
+      backgroundImage: {
+        cta: "url('/cta/bg.svg')",
+        'hero-about': "url('/about/hero.png')",
+        'hero-sermon': "url('/sermons/hero.png')",
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
