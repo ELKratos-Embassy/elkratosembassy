a31ce0a60b0aad4133f8cfb58df9966a615bede2
Yunus Habeeb <habeebdh1@gmail.com>
2025-01-03 17:07:52 +0100

    Add sermons data structure for upcoming events

---
+++ constants/sermons.ts
@@ -0,0 +1,136 @@
+const sermons = [
+  {
+    id: '1',
+    date: {
+      caption: '20',
+      text: 'July',
+      year: '2018',
+    },
+    info: {
+      desc: {
+        caption: 'Upcoming event',
+        text: 'Watch and listen to our sermons',
+        message: [
+          ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam viverra orci sagittis eu volutpat.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+        ],
+      },
+      time: {
+        icon: {
+          src: '/sermons/clock.svg',
+          alt: 'Clock',
+        },
+        text: ['Friday 23:39 IST', 'Saturday 11:20 ISD'],
+      },
+      location: {
+        icon: {
+          src: '/sermons/location.svg',
+          alt: 'Location',
+        },
+        text: ['No 233 Main St. New York,', 'United States'],
+      },
+    },
+  },
+  {
+    id: '2',
+    date: {
+      caption: '20',
+      text: 'July',
+      year: '2018',
+    },
+    info: {
+      desc: {
+        caption: 'Upcoming event',
+        text: 'Watch and listen to our sermons',
+        message: [
+          ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam viverra orci sagittis eu volutpat.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+        ],
+      },
+      time: {
+        icon: {
+          src: '/sermons/clock.svg',
+          alt: 'Clock',
+        },
+        text: ['Friday 23:39 IST', 'Saturday 11:20 ISD'],
+      },
+      location: {
+        icon: {
+          src: '/sermons/location.svg',
+          alt: 'Location',
+        },
+        text: ['No 233 Main St. New York,', 'United States'],
+      },
+    },
+  },
+  {
+    id: '3',
+    date: {
+      caption: '20',
+      text: 'July',
+      year: '2018',
+    },
+    info: {
+      desc: {
+        caption: 'Upcoming event',
+        text: 'Watch and listen to our sermons',
+        message: [
+          ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam viverra orci sagittis eu volutpat.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+        ],
+      },
+      time: {
+        icon: {
+          src: '/sermons/clock.svg',
+          alt: 'Clock',
+        },
+        text: ['Friday 23:39 IST', 'Saturday 11:20 ISD'],
+      },
+      location: {
+        icon: {
+          src: '/sermons/location.svg',
+          alt: 'Location',
+        },
+        text: ['No 233 Main St. New York,', 'United States'],
+      },
+    },
+  },
+  {
+    id: '4',
+    date: {
+      caption: '20',
+      text: 'July',
+      year: '2018',
+    },
+    info: {
+      desc: {
+        caption: 'Upcoming event',
+        text: 'Watch and listen to our sermons',
+        message: [
+          ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui faucibus in ornare quam viverra orci sagittis eu volutpat.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Duifaucibus in ornare quam viverra orci sagittis euvolutpat. Pharetra sit amet aliquam id diam maecenas ultricies.',
+        ],
+      },
+      time: {
+        icon: {
+          src: '/sermons/clock.svg',
+          alt: 'Clock',
+        },
+        text: ['Friday 23:39 IST', 'Saturday 11:20 ISD'],
+      },
+      location: {
+        icon: {
+          src: '/sermons/location.svg',
+          alt: 'Location',
+        },
+        text: ['No 233 Main St. New York,', 'United States'],
+      },
+    },
+  },
+];
+
+export default sermons;
